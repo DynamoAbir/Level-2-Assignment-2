a07a2bb0c70420cbc9d6366d195f851ee0fe707a
DynamoAbir <mhabir10@gmail.com>
2024-07-05 17:20:36 +0600

    removing console

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ main().catch((err) => console.log(err));
 async function main() {
   try {
     await mongoose.connect(process.env.DB_URL as string);
-    console.log("Database Connected");
+
     app.listen(port, () => {
       console.log(`Example app listening on port ${process.env.PORT}`);
     });
